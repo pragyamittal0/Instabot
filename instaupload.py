8623aab0176bab565cca37ec1167d546e055bb9a
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2021-06-01 14:27:17 +0530

    Update instaupload.py

--- instaupload.py
+++ instaupload.py
@@ -1,6 +1,6 @@
 from instabot import Bot
 bot = Bot()
-bot.login(username="_pragya_mittal181_", password="gautam123")
+bot.login(username="_pragya_mittal181_", password="*******")
 
 bot.upload_photo("p.jpeg" , caption="We all start from strangers.")
 
